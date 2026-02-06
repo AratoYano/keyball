47346ade1059c02d55558ede50c1cbc82b7a8b86
AratoYano <44079345+AratoYano@users.noreply.github.com>
2026-02-06 13:10:34 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/rules.mk
@@ -1,7 +1,13 @@
-RGBLIGHT_ENABLE = yes
+# Keyball固有の機能を有効にする（これでオートマウスが復活します）
+KEYBALL_ENABLE = yes
 
-OLED_ENABLE = yes
+# OLED（液晶）をオフにして、コンボ用の容量を確保します
+OLED_ENABLE = no
 
-VIA_ENABLE = yes
+# コンボ機能とRemap（VIA）連携を有効にします
 COMBO_ENABLE = yes
 VIA_COMBO_ENABLE = yes
+
+# その他の基本設定
+VIA_ENABLE = yes
+LTO_ENABLE = yes  # 容量をさらに圧縮する設定です
