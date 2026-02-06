abea38261f6008c87c6928aa38dc587e7b9c5e12
AratoYano <44079345+AratoYano@users.noreply.github.com>
2026-02-06 13:45:33 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/rules.mk
@@ -1,14 +1,11 @@
-# Keyballの基本機能を有効にします（オートマウス復活のため）
 KEYBALL_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes
 
-# OLEDを物理的にオフにするのではなく、
-# 「描画処理」だけをスキップさせることで容量を空けつつエラーを回避します
-OLED_ENABLE = yes
+# 液晶をオフにして、コンボ機能とRemap通信用の容量を確保します
+OLED_ENABLE = no
 OLED_DRIVER_ENABLE = no
 
-# コンボ機能とRemap連携を有効にします
+# コンボ機能を有効にし、Remap（VIA）のメニューに出現させます
 COMBO_ENABLE = yes
 VIA_COMBO_ENABLE = yes
-
-# 容量圧縮のための設定
-LTO_ENABLE = yes
